1d5d8d25409e1d2ce9dc53ccc2a21dbe7f21505b
Justin Weberg <justin.weberg@gmail.com>
2020-05-27 14:36:57 -0500

    completed lesson

--- server.js
+++ server.js
@@ -20,8 +20,18 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/timestamp', (req, res) => {
+  const date = new Date();
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  const date = new Date(req.params.date_string);
+  if(date.toString() === 'Invalid Date') {
+    res.json({"error": "Invalid Date"});
+  } else {
+    res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+  }
 });
 
 
